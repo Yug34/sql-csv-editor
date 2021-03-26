d989e0e3b697e1cf3452318d22e5c83b98ea54dc
Yug34 <vertilax1337@gmail.com>
2021-03-26 19:35:39 +0530

    Added drag and drop functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import {useState} from "react/cjs/react.production.min";
+import React, {useEffect, useState} from "react"
 
 function App() {
+  let [text, setText] = useState(null);
+
+  useEffect(() => {
+    console.log("File recieved:\n" + text);
+  }, [text]);
+
+  function dropHandler(e) {
+    e.preventDefault();
+    let file = e.dataTransfer.files[0];
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+      // console.log(e.target.result);
+      setText(e.target.result);
+    };
+    reader.readAsText(file);
+  }
+
+  function dragOverHandler(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drop_zone" onDrop={(e) => dropHandler(e)} onDragOver={e => dragOverHandler(e)}>
+      <p>Drag a file here!</p>
+      {text?<p>{text}</p>:<p>Drag a file here yo!</p>}
     </div>
   );
 }
