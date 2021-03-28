cb281265e7e80794d583c55590c3b5047e7c1a85
Yug34 <vertilax1337@gmail.com>
2021-03-28 14:19:25 +0530

    Results displayed correctly (Hardcoded)

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -3,17 +3,52 @@ import React, {useEffect, useState} from "react";
 import "../css/Results.css";
 
 function Results(props) {
-  let [resultList, setResultList] = useState(<div>Result!</div>);
+  let [resultList, setResultList] = useState(null);
+  let [header, setHeader] = useState(null);
 
   useEffect(() => {
     if (props.results) {
+      setHeader(Object.keys(props.results[0]).map(key => (
+          <th key={key}>{key}</th>
+      )));
+      // setHeader(Object.keys(props.results[0]));
+
+      // for (let key of Object.keys(props.results[0])) {
+      //
+      // }
+
       setResultList(
-          props.results.map((result) => <div key={result.orderID}>abc!</div>)
+        props.results.map((result) => (
+            <tr key={result.orderID}>
+              <th>{result.orderID}</th>
+              <th>{result.productID}</th>
+              <th>{result.unitPrice}</th>
+              <th>{result.quantity}</th>
+              <th>{result.discount}</th>
+            </tr>
+        ))
       );
     }
+
   }, [props.results]);
 
-  return <div>{resultList}</div>;
+  // useEffect(() => {
+  //
+  // }, [header])
+
+  return (
+  <div>
+    <table>
+      <thead>
+        <tr>
+          {header}
+        </tr>
+      </thead>
+      <tbody>
+        {resultList}
+      </tbody>
+    </table>
+  </div>);
 }
 
 export default Results;
