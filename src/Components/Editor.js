da6ac43ddcd7ae869762e095d1e7ac6871fa4994
Yug34 <vertilax1337@gmail.com>
2021-03-31 22:11:54 +0530

    [app/layout] Added block titles, styling changes, scrollbar change

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -7,32 +7,43 @@ import "ace-builds/src-noconflict/theme-monokai";
 
 function Editor(props) {
   return (
-    <div className="editor">
-      <AceEditor
-        className="editorInput"
-        placeholder="Enter SQL Query here"
-        mode="sql"
-        theme="monokai"
-        name="blah2"
-        fontSize={14}
-        showPrintMargin={true}
-        showGutter={true}
-        highlightActiveLine={true}
-        onChange={(e) => props.queryChangeHandler(e)}
-        value={props.query}
-        setOptions={{
-          enableBasicAutocompletion: false,
-          enableLiveAutocompletion: false,
-          enableSnippets: false,
-          showLineNumbers: true,
-          wrap: true,
-          hScrollBarAlwaysVisible: false,
-          vScrollBarAlwaysVisible: false,
-          tabSize: 2,
-          cursorStyle: "smooth"
+    <div>
+      <div
+        style={{
+          "text-align": "center",
+          color: "white",
+          background: "#555651",
         }}
-        style={{ height: "25vh", width: "50vw" }}
-      />
+      >
+        SQL Editor
+      </div>
+      <div className="editor">
+        <AceEditor
+          className="editorInput"
+          placeholder="Enter SQL Query here"
+          mode="sql"
+          theme="monokai"
+          name="blah2"
+          fontSize={14}
+          showPrintMargin={true}
+          showGutter={true}
+          highlightActiveLine={true}
+          onChange={(e) => props.queryChangeHandler(e)}
+          value={props.query}
+          setOptions={{
+            enableBasicAutocompletion: false,
+            enableLiveAutocompletion: false,
+            enableSnippets: false,
+            showLineNumbers: true,
+            wrap: true,
+            hScrollBarAlwaysVisible: false,
+            vScrollBarAlwaysVisible: false,
+            tabSize: 2,
+            cursorStyle: "smooth",
+          }}
+          style={{ height: "25vh", width: "50vw" }}
+        />
+      </div>
     </div>
   );
 }
--- src/Components/ErrorLogger.js
+++ src/Components/ErrorLogger.js
@@ -1,45 +1,55 @@
 // import {useState} from "react/cjs/react.production.min";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../css/ErrorLogger.css";
 import AceEditor from "react-ace";
 
 function ErrorLogger(props) {
-    let [err, setErr] = useState("")
+  let [err, setErr] = useState("");
 
-    useEffect(() => {
-        if (props.error === "" || props.error === null) {
-            setErr("");
-        }
-        else {
-            setErr(props.error);
-        }
-    }, [props.error])
+  useEffect(() => {
+    if (props.error === "" || props.error === null) {
+      setErr("");
+    } else {
+      setErr(props.error);
+    }
+  }, [props.error]);
 
-    return (
-        <div className="errorDisplay">
-            <AceEditor
-                className="error"
-                placeholder="Error Logs:"
-                mode="sql"
-                theme="monokai"
-                name="errorBlah"
-                fontSize={14}
-                readOnly={true}
-                showPrintMargin={true}
-                showGutter={true}
-                value={err}
-                setOptions={{
-                    enableBasicAutocompletion: false,
-                    enableSnippets: false,
-                    wrap: true,
-                    hScrollBarAlwaysVisible: false,
-                    vScrollBarAlwaysVisible: false,
-                    cursorStyle: "smooth"
-                }}
-                style={{ height: "25vh", width: "50vw" }}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <div
+        style={{
+          "text-align": "center",
+          color: "white",
+          background: "#555651",
+        }}
+      >
+        Error Logger
+      </div>
+      <div className="errorDisplay">
+        <AceEditor
+          className="error"
+          placeholder="Error Logs:"
+          mode="sql"
+          theme="monokai"
+          name="errorBlah"
+          fontSize={14}
+          readOnly={true}
+          showPrintMargin={true}
+          showGutter={true}
+          value={err}
+          setOptions={{
+            enableBasicAutocompletion: false,
+            enableSnippets: false,
+            wrap: true,
+            hScrollBarAlwaysVisible: false,
+            vScrollBarAlwaysVisible: false,
+            cursorStyle: "smooth",
+          }}
+          style={{ height: "25vh", width: "50vw" }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ErrorLogger;
